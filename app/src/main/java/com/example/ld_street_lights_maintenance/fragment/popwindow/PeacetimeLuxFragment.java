efb20f39978ccffed8a0c5f402e4687babcbb5c6
liu <602694077@qq.com>
2021-03-24 16:19:08 +0800

    2021年3月24日 16:18:59 界面基本完成

--- app/src/main/java/com/example/ld_street_lights_maintenance/fragment/popwindow/PeacetimeLuxFragment.java
+++ app/src/main/java/com/example/ld_street_lights_maintenance/fragment/popwindow/PeacetimeLuxFragment.java
@@ -1,6 +1,7 @@
 package com.example.ld_street_lights_maintenance.fragment.popwindow;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,11 +11,25 @@ import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 
 import com.example.ld_street_lights_maintenance.R;
+import com.example.ld_street_lights_maintenance.base.BaseFragment;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 
 @SuppressLint("ValidFragment")
-public class PeacetimeLuxFragment extends Fragment {
+public class PeacetimeLuxFragment extends BaseFragment implements OnChartValueSelectedListener {
     private String mTitle;
+    private LineChart lineChart;
 
     public static PeacetimeLuxFragment getInstance(String title) {
         PeacetimeLuxFragment sf = new PeacetimeLuxFragment();
@@ -29,10 +44,165 @@ public class PeacetimeLuxFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fr_simple_card, null);
-        TextView card_title_tv = (TextView) v.findViewById(R.id.card_title_tv);
-        card_title_tv.setText(mTitle);
 
-        return v;
+        View rootView = inflater.inflate(R.layout.fr_peacetime_lux, null);
+
+        initView(rootView);
+
+        return rootView;
+    }
+
+    private void initView(View rootView) {
+        initChart(rootView);
+    }
+
+    private void initChart(View rootView) {
+        lineChart = rootView.findViewById(R.id.chart_peacetime_lux);
+        lineChart.setOnChartValueSelectedListener(this);
+        // no description text
+        lineChart.getDescription().setEnabled(false);
+        // enable touch gestures
+        lineChart.setTouchEnabled(true);
+        lineChart.setDragDecelerationFrictionCoef(0.9f);
+
+        // get the legend (only possible after setting data)
+        Legend l = lineChart.getLegend();
+
+        // modify the legend ...
+        l.setForm(Legend.LegendForm.LINE);
+      //  l.setTypeface(tfLight);
+        l.setTextSize(11f);
+        l.setTextColor(Color.WHITE);
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setDrawInside(false);
+//        l.setYOffset(11f);
+
+        XAxis xAxis = lineChart.getXAxis();
+      //  xAxis.setTypeface(tfLight);
+        xAxis.setTextSize(11f);
+        xAxis.setTextColor(Color.WHITE);
+        xAxis.setDrawGridLines(false);
+        xAxis.setDrawAxisLine(false);
+        xAxis.setEnabled(false);
+
+        YAxis leftAxis = lineChart.getAxisLeft();
+      //  leftAxis.setTypeface(tfLight);
+        leftAxis.setTextColor(ColorTemplate.getHoloBlue());
+        leftAxis.setAxisMaximum(200f);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setDrawGridLines(true);
+        leftAxis.setGranularityEnabled(true);
+
+        YAxis rightAxis = lineChart.getAxisRight();
+       // rightAxis.setTypeface(tfLight);
+        rightAxis.setTextColor(Color.RED);
+        rightAxis.setAxisMaximum(900);
+        rightAxis.setAxisMinimum(-200);
+        rightAxis.setDrawGridLines(false);
+        rightAxis.setDrawZeroLine(false);
+        rightAxis.setGranularityEnabled(false);
+
+        // 设置图表数据
+        setData(7,55);
+    }
+
+
+    private void setData(int count, float range) {
+
+        ArrayList<Entry> values1 = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            float val = (float) (Math.random() * (range / 2f)) + 50;
+            values1.add(new Entry(i, val));
+        }
+
+        ArrayList<Entry> values2 = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            float val = (float) (Math.random() * range) + 60;
+            values2.add(new Entry(i, val));
+        }
+
+        ArrayList<Entry> values3 = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            float val = (float) (Math.random() * range) + 500;
+            values3.add(new Entry(i, val));
+        }
+
+        LineDataSet set1, set2, set3;
+
+        if (lineChart.getData() != null &&
+                lineChart.getData().getDataSetCount() > 0) {
+            set1 = (LineDataSet) lineChart.getData().getDataSetByIndex(0);
+            set2 = (LineDataSet) lineChart.getData().getDataSetByIndex(1);
+            set3 = (LineDataSet) lineChart.getData().getDataSetByIndex(2);
+            set1.setValues(values1);
+            //set2.setValues(values2);
+            set3.setValues(values3);
+            lineChart.getData().notifyDataChanged();
+            lineChart.notifyDataSetChanged();
+        } else {
+            // create a dataset and give it a type
+            set1 = new LineDataSet(values1, "DataSet 1");
+
+            set1.setAxisDependency(YAxis.AxisDependency.LEFT);
+            set1.setColor(ColorTemplate.getHoloBlue());
+            set1.setCircleColor(Color.WHITE);
+            set1.setLineWidth(2f);
+            set1.setCircleRadius(3f);
+            set1.setFillAlpha(65);
+            set1.setFillColor(ColorTemplate.getHoloBlue());
+            set1.setHighLightColor(Color.rgb(244, 117, 117));
+            set1.setDrawCircleHole(false);
+            //set1.setFillFormatter(new MyFillFormatter(0f));
+            //set1.setDrawHorizontalHighlightIndicator(false);
+            //set1.setVisible(false);
+            //set1.setCircleHoleColor(Color.WHITE);
+
+            // create a dataset and give it a type
+          /*  set2 = new LineDataSet(values2, "DataSet 2");
+            set2.setAxisDependency(YAxis.AxisDependency.RIGHT);
+            set2.setColor(Color.RED);
+            set2.setCircleColor(Color.WHITE);
+            set2.setLineWidth(2f);
+            set2.setCircleRadius(3f);
+            set2.setFillAlpha(65);
+            set2.setFillColor(Color.RED);
+            set2.setDrawCircleHole(false);
+            set2.setHighLightColor(Color.rgb(244, 117, 117));*/
+            //set2.setFillFormatter(new MyFillFormatter(900f));
+
+            set3 = new LineDataSet(values3, "DataSet 3");
+            set3.setAxisDependency(YAxis.AxisDependency.RIGHT);
+            set3.setColor(Color.YELLOW);
+            set3.setCircleColor(Color.WHITE);
+            set3.setLineWidth(2f);
+            set3.setCircleRadius(3f);
+            set3.setFillAlpha(65);
+            set3.setFillColor(ColorTemplate.colorWithAlpha(Color.YELLOW, 200));
+            set3.setDrawCircleHole(false);
+            set3.setHighLightColor(Color.rgb(244, 117, 117));
+
+            // create a data object with the data sets
+           // LineData data = new LineData(set1, set2, set3);
+            LineData data = new LineData(set1, set3);
+            data.setValueTextColor(Color.WHITE);
+            data.setValueTextSize(9f);
+
+            // set data
+            lineChart.setData(data);
+        }
+    }
+
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+
+    }
+
+    @Override
+    public void onNothingSelected() {
+
     }
 }
